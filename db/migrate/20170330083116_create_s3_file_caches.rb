c20872d16953a477463f419b764fc9d655186de2
wat-aro <kazutas1008@gmail.com>
2017-03-30 17:36:17 +0900

    bin/rails g model s3_file_cache s3_full_path:string

---
+++ db/migrate/20170330083116_create_s3_file_caches.rb
@@ -0,0 +1,9 @@
+class CreateS3FileCaches < ActiveRecord::Migration[5.0]
+  def change
+    create_table :s3_file_caches do |t|
+      t.string :s3_full_path, null:false
+
+      t.timestamps null:false
+    end
+  end
+end

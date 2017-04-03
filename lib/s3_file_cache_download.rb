44ba88e5cf315098120cfbcf40854882b21ad98d
taki3 <taki3@users.noreply.github.com>
2017-04-03 16:43:48 +0900

    ファイルの有効期限が過ぎていた場合は、再ダウンロードをするようにした

--- lib/s3_file_cache_download.rb
+++ lib/s3_file_cache_download.rb
@@ -35,11 +35,27 @@ module S3FileCacheDownload
   module Helper
     def send_s3_file(bucket_name, path, option = {})
       S3FileCache.transaction do
-        s3_file_cache = S3FileCache.find_or_create_by(s3_full_path: path, bucket_name: bucket_name)
-        s3_file_cache.fetch!
+        s3_file_cache = S3FileCache.find_by(s3_full_path: path, bucket_name: bucket_name)
+
+        if s3_file_cache.present?
+          if s3_file_cache.expire?
+            FileUtils.rm(s3_file_cache.place)
+            s3_file_cache.destroy
+
+            download_file_on_s3(path, bucket_name)
+          else
+          end
+        else
+          download_file_on_s3(path, bucket_name)
+        end
 
         send_file s3_file_cache.place, {filename: s3_file_cache.filename}.merge(option)
       end
+    end
+
+    def download_file_on_s3(path, bucket_name)
+      cache = SS3FileCache.create!(s3_full_path: path, bucket_name: bucket_name)
+      cache.fetch!
     end
   end
 end
